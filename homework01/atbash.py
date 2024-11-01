34c2786350d9628abe8cf49440bfa246a3f0dc26
Panas <nastusha.panas@gmail.com>
2024-11-01 13:15:39 +0300

    Выпполнила функцию encrypt_atbash()

---
+++ homework01/atbash.py
@@ -0,0 +1,14 @@
+def encrypt_atbash(plaintext):
+    alphabet = '0абвгдежзийклмнопрстуфхцчшщъыьэюя'
+    ciphertext = ""
+
+    for char in plaintext:
+        if char.lower() in alphabet:
+            i = alphabet.index(char.lower())
+            if char.isupper():
+                ciphertext += alphabet[-i].upper()
+            else:
+                ciphertext += alphabet[-i]
+        else:
+            ciphertext += char
+    return ciphertext
